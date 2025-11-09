ccc931bee2496cd13fe311e3753ed1327efb525e
Vcoch27 <hoangtungmy123@gmail.com>
2025-11-09 09:43:31 +0700

    chore(build): setup Gradle multi-module with JDK 21 (server/client skeleton)

---
+++ client/src/main/java/vn/vku/kanban/client/Main.java
@@ -0,0 +1,7 @@
+package vn.vku.kanban.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[Client] Hello. (JavaFX UI will come later)");
+    }
+}
---
+++ server/src/main/java/vn/vku/kanban/server/Main.java
@@ -0,0 +1,7 @@
+package vn.vku.kanban.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[Server] Hello. (TCP will listen in next step)");
+    }
+}
